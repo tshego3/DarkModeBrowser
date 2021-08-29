6d1648bac98064c8f04230ed2ab5f07aee338704
Tshegofatso Motswagae <tshegofatsomotswagae@hotmail.com>
2021-08-29 23:03:32 +0200

    DOM Element Update: code

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -8,6 +8,11 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         </style>`
     );
 
+    $("html, pre").css({ "background": "#1A1A1A" });
+    $("div, h1, h2, h3, p, textarea, tr, td, th").css({ "background-color": "#1A1A1A", "color": "#E5E5E5" });
+    $("code, span").css({ "background": "#1A1A1A", "color": "#E5E5E5"});
+    $("a").css({ "color": "#E5E5E5"});
+
     var element = document.body;
     element.classList.toggle("dark-mode");
 
--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,11 @@
-const sendMessageId = document.getElementById("sendmessageid");
-if (sendMessageId) {
-    sendMessageId.onclick = function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                {
-                    tabId: tabs[0].id
-                },
-                function(response) {
-                    window.close();
-                }
-            );
-        });
-    };
-}
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+            tabId: tabs[0].id
+        },
+        function(response) {
+            window.close();
+        }
+    );
+});
